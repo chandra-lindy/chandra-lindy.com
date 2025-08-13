85cb62b11954d30773f8cfd13d2da1d99f098861
Chandra Lindy <chandra.lindy@csu.fullerton.edu>
2025-08-12 19:49:38 -0700

    add blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function BlogPage() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-black text-white">
+      <h1 className="text-6xl text-center">Blog Under Construction</h1>
+    </div>
+  );
+}
