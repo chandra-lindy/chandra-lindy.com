c9bc601191ae10827b668d6634db753b824ef8e0
Chandra Lindy <chandra.lindy@csu.fullerton.edu>
2025-10-01 13:56:06 -0700

    add hero section to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-black text-white z-10">
-      <h1 className="text-6xl text-center">Under Construction</h1>
+      <div className="text-center">
+        <h1 className="text-6xl mb-8">Code, Commit, Conquer: My Learning Log</h1>
+        <a href="/blog" className="px-6 py-3 bg-white text-black rounded-lg hover:bg-gray-200 transition-colors">
+          Explore My Blog
+        </a>
+      </div>
     </div>
   );
 }
